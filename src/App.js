11a4d6e45af442a5d782a643895af7e00b774799
Jorge de Jesus Tejeda Bello <jtejeda@Jorges-Mac-mini-2.local>
2022-06-25 18:11:30 -0400

    Create Card component, a giving style inside Services component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import Intro from './components/Intro/Intro'
 import Navbar from './components/Navbar/Navbar'
+import Services from './components/Services/Services'
 
 const App = () => {
   return (
     <div className='App'>
       <Navbar />
       <Intro />
+      <Services />
     </div>
   )
 }
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import HeartEmoji from '../../img/heartemoji.png';
+import Card from '../Card/Card';
+
+import Glasses from '../../img/glasses.png';
+import Humble from '../../img/humble.png';
+
+import Resume from './Resume.pdf';
+
+import './Services.css';
+
+const Services = () => {
+  return (
+    <div className='Services'>
+      <div className='awesome'>
+        <span>My Awesome</span>
+        <span>services</span>
+        <span>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          <br />
+          Dolores corrupti quo temporibus unde quam? Nostrum atque amet
+          obcaecati.
+        </span>
+        <a href={Resume} download>
+          <button className='button s-button'>Download CV</button>
+        </a>
+        <div className='blur s-blur1' style={{ background: '#abf1ff94' }}></div>
+      </div>
+      <div className='cards'>
+        <div style={{ left: '14rem' }}>
+          <Card
+            emoji={HeartEmoji}
+            heading='Design'
+            detail='Figma, Sketch, Photoshop, Adobe Xd, Adobe'
+          />
+        </div>
+        <div style={{ left: '-4rem', top: '12rem' }}>
+          <Card
+            emoji={Glasses}
+            heading='Developer'
+            detail='Html, Css, Javascript, React'
+          />
+        </div>
+        <div style={{ left: '12rem', top: '19rem' }}>
+          <Card
+            emoji={Humble}
+            heading='UI/UX'
+            detail='Dolores corrupti quo temporibus unde quam? Nostrum atque.'
+          />
+        </div>
+        <div
+          className='blur s-blur2'
+          style={{ background: 'var(--purple)' }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
